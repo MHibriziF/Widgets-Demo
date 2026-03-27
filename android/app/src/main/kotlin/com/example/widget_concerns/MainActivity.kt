e87d96c6342b35c51ec3ddbf473c53a79245c856
MHibriziF <hibrizifarghana@gmail.com>
2026-03-27 11:45:26 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/widget_concerns/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.widget_concerns
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WidgetConcernsApp extends StatelessWidget {
+  const WidgetConcernsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Widget Concerns',
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
